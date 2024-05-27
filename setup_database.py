c6975d9e25b54106479a524e480f9597185bd26c
Zion <115614610+zieto22@users.noreply.github.com>
2024-05-27 08:52:34 +0000

    docstrings

--- setup_database.py
+++ setup_database.py
@@ -3,5 +3,9 @@ from app import app, db
 from models import User, Todo
 
 with app.app_context():
+    """
+    This script sets up the database and creates the necessary tables.
+    """
+
     db.create_all()
     print("Database and tables created.")
